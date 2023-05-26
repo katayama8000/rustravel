92077e936e7036cad6cd8d694abbef2452609e97
katayama8000 <tattu.0310@gmail.com>
2023-05-26 21:41:19 +0900

    rust

---
+++ guessing_game/README.md
@@ -0,0 +1,1 @@
+https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");          // 数を当ててごらん
+    println!("Guess the number!");
 
-    println!("Please input your guess.");   // ほら、予想を入力してね
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");     // 行の読み込みに失敗しました
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);     // 次のように予想しました: {}
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        // 予測と秘密の数字を比較して、それらが等しいかどうかを確認する
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }

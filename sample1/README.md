a66ef4cfb203c37aaeb447b247a64e21dfca821c
katayama8000 <tattu.0310@gmail.com>
2023-05-26 21:22:22 +0900

    update

---
+++ sample1/README.md
@@ -0,0 +1,12 @@
+# rust-travel
+
+## コンパイル　& 実行
+```bash
+$ rustc src/main.rs
+$ ./main
+```
+
+## 依存関係を解決して実行
+```bash
+$ cargo run
+```
---
+++ sample1/src/main.rs
@@ -0,0 +1,16 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    // let stdout = stdout();
+    // let message = String::from("Hello fellow Rustaceans!");
+    // let width = message.chars().count();
+
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(message.as_bytes(), width, &mut writer).unwrap();
+    let a = 10;               // immutable object
+    let aref1 = &a;           // reference
+    let aref2 = &a;           // reference
+    println!("{}, {}, {}", a, aref1, aref2); // borrow check!! - OK
+}
+    

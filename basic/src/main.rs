fe32643becb61769dd5b59d935a576187d289b24
katayama8000 <tattu.0310@gmail.com>
2023-06-16 22:17:30 +0900

    test

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,9 +1,83 @@
 fn main() {
-    println!("何か文字を入力して下さい:");
+    // test
+}
 
-    let mut word = String::new();
-    std::io::stdin().read_line(&mut word).ok();
-    let answer = word.trim().to_string();
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-    println!("{}", answer);
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[cfg(test)]
+mod test {
+    // plus
+    #[test]
+    fn it_works1() {
+        assert_eq!(2 + 2, 4);
+    }
+    // minus
+    #[test]
+    fn it_works2() {
+        assert_eq!(5 - 2, 3);
+    }
+
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            //挨拶(greeting)は名前を含んでいません。その値は`{}`でした
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
 }

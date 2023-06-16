7f53d1b396070991d361312dc8fb6631dee78aea
katayama8000 <tattu.0310@gmail.com>
2023-06-16 22:33:58 +0900

    struct

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,83 +1,48 @@
-fn main() {
-    // test
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
+struct Human {
+    name: String,
+    age: u32,
     height: u32,
+    weight: u32,
 }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-pub fn add_two(a: i32) -> i32 {
-    a + 2
-}
-
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
-}
-
-#[cfg(test)]
-mod test {
-    // plus
-    #[test]
-    fn it_works1() {
-        assert_eq!(2 + 2, 4);
-    }
-    // minus
-    #[test]
-    fn it_works2() {
-        assert_eq!(5 - 2, 3);
+impl Human {
+    fn new(name: String, age: u32, height: u32, weight: u32) -> Self {
+        Self {
+            name,
+            age,
+            height,
+            weight,
+        }
     }
 
-    use super::*;
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            //挨拶(greeting)は名前を含んでいません。その値は`{}`でした
-            "Greeting did not contain name, value was `{}`",
-            result
+    fn print(&self) {
+        println!(
+            "Name: {}, Age: {}, Height: {}, Weight: {}",
+            self.name, self.age, self.height, self.weight
         );
     }
+}
 
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
+struct Color(i32, i32, i32);
 
-        assert!(!smaller.can_hold(&larger));
+impl Color {
+    fn print(&self) {
+        println!("R: {}, G: {}, B: {}", self.0, self.1, self.2);
     }
+}
 
-    #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
-    }
+fn main() {
+    let human: Human = Human::new(String::from("John"), 20, 170, 60);
+    human.print();
+
+    let tom: Human = Human {
+        name: String::from("Tom"),
+        age: 25,
+        ..human
+    };
+
+    tom.print();
+
+    let color: Color = Color(255, 255, 255);
+    color.print();
 }

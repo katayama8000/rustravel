0a7b66892d86ed8b17e3b56ef7d3be9e558c7c47
katayama8000 <tattu.0310@gmail.com>
2023-06-18 12:04:05 +0900

    itr

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -2,6 +2,84 @@ fn add_one_v1(x: i32) -> i32 {
     x + 1
 }
 
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let shoes: Vec<Shoe> = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+    let in_my_size: Vec<Shoe> = shoes_in_my_size(shoes, 10);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ]
+    );
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    // 関連型
+    type Item = u32;
+    // nextメソッド
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            // Someは値を持つ
+            Some(self.count)
+        } else {
+            // Noneは値を持たない
+            None
+        }
+    }
+}
+
+#[test]
+fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(x, y)| x * y)
+        .filter(|x: &u32| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
+}
+
 fn main() {
     // クロージャー
     let add_one_v2 = |x: i32| -> i32 { x + 1 };
@@ -26,7 +104,7 @@ fn main() {
     }
 
     let v: Vec<i32> = vec![1, 2, 3];
-    let doubled: Vec<i32> = v.iter().map(|x| x * 2).collect();
+    let doubled: Vec<i32> = v.iter().map(|x: &i32| x * 2).collect();
     println!("{:?}", doubled);
 
     let add_world_to_vec = |v: &mut Vec<String>| {
@@ -41,4 +119,8 @@ fn main() {
     let mut s: String = String::from("hello");
     add_world_to_string(&mut s);
     println!("{}", s);
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<i32> = v1.iter().map(|x: &i32| x + 1).collect();
+    println!("{:?}", v2);
 }

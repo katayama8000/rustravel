59eb19656c1119f821d78541f200d258e262c4e9
katayama8000 <tattu.0310@gmail.com>
2023-07-21 00:52:55 +0900

    mod

---
+++ basic/src/lib02/multiply.rs
@@ -0,0 +1,7 @@
+pub fn multi_numbers(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+pub fn divide_numbers(a: i32, b: i32) -> i32 {
+    a / b
+}
--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+mod lib01;
+use lib01::add::add_numbers;
+use rand::Rng;
+mod calc;
+mod lib02;
+use lib02::multiply::divide_numbers;
+use lib02::multiply::multi_numbers;
+
+fn main() {
+    println!("1 + 2 = {}", add_numbers(1, 2));
+    let mut rng = rand::thread_rng();
+    let n1: i32 = rng.gen();
+    let n2: i32 = rng.gen();
+    println!("{} + {} = {}", n1, n2, add_numbers(n1, n2));
+    println!("{} + {} = {}", n1, n2, calc::add(n1, n2));
+    let num1: i32 = rng.gen();
+    let num2: i32 = rng.gen();
+    println!("{} * {} = {}", 3, 2, multi_numbers(3, 2));
+    println!("{} * {} = {}", 6, 2, divide_numbers(6, 2));
+}
+
+// calc モジュールを定義する
+// mod calc {
+//     pub fn add(a: i32, b: i32) -> i32 {
+//         a + b
+//     }
+// }

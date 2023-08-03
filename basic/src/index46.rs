14087e4f3900292bddfa9213f9aa5041bf7f4d3f
katayama8000 <tattu.0310@gmail.com>
2023-08-04 00:21:57 +0900

    result

---
+++ basic/src/index46.rs
@@ -0,0 +1,22 @@
+use std::num::ParseIntError;
+
+fn main() -> Result<(), ParseIntError> {
+    let number_str = "10";
+    let number = match number_str.parse::<i32>() {
+        Ok(number) => number,
+        Err(e) => return Err(e),
+    };
+    println!("{}", number);
+
+    let twenty: i32 = multiply("10", "2");
+    println!("double is {}", twenty);
+    Ok(())
+}
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> i32 {
+    // Let's try using `unwrap()` to get the number out. Will it bite us?
+    // `unwrap()`で数字を取り出してみましょう。痛い目を見るでしょうか？
+    let first_number = first_number_str.parse::<i32>().unwrap();
+    let second_number = second_number_str.parse::<i32>().unwrap();
+    first_number * second_number
+}
--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,22 +1,39 @@
 use std::num::ParseIntError;
 
-fn main() -> Result<(), ParseIntError> {
-    let number_str = "10";
-    let number = match number_str.parse::<i32>() {
-        Ok(number) => number,
-        Err(e) => return Err(e),
-    };
-    println!("{}", number);
+// Define a generic alias for a `Result` with the error type `ParseIntError`.
+// `ParseIntError`を`Err`の型として持つ全ての`Result`のジェネリックエイリアス
+type AliasedResult<T> = Result<T, ParseIntError>;
 
-    let twenty: i32 = multiply("10", "2");
-    println!("double is {}", twenty);
-    Ok(())
+// Use the above alias to refer to our specific `Result` type.
+// 上で定義したエイリアス（この場所特有の`Result`型）を使用
+fn multiply(first_number_str: &str, second_number_str: &str) -> AliasedResult<i32> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str
+            .parse::<i32>()
+            .map(|second_number| first_number * second_number)
+    })
 }
 
-fn multiply(first_number_str: &str, second_number_str: &str) -> i32 {
-    // Let's try using `unwrap()` to get the number out. Will it bite us?
-    // `unwrap()`で数字を取り出してみましょう。痛い目を見るでしょうか？
-    let first_number = first_number_str.parse::<i32>().unwrap();
-    let second_number = second_number_str.parse::<i32>().unwrap();
-    first_number * second_number
+// Here, the alias again allows us to save some space.
+// もう一度使用。エイリアスによって再度明記する必要性がない。
+fn print(result: AliasedResult<i32>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    print(multiply("10", "2"));
+    print(multiply("t", "2"));
+
+    let opt = Some(10);
+    let num: Result<i32, &str> = Ok(10);
+    match num {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+
+    let str: &str = "10";
+    let _ret: Result<i32, ParseIntError> = str.parse();
 }
